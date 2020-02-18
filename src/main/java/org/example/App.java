5aa3fb4363c299a05b5f9d7d284158dc89f20514
Nishant <nishantsaini97286@gmail.com>
2020-02-18 17:12:44 +0530

    Task files

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import java.io.PrintStream;
+
+/**
+ * Hello world!
+ *
+ */
+
+public class App 
+{
+    static{
+        System.out.format("%s","SI and CI calculator \n");
+        SimpleInterest s=new SimpleInterest();
+        s.setData(1000,2,5);
+        s.calculateAndDisplay();
+        CompoundInterest c=new CompoundInterest();
+        c.setData(1000,2,5);
+        c.calculateAndDisplay();
+    }
+    public static void main( String[] args )
+    {
+
+    }
+}
